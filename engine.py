184985f3dbae888859ee403fff9782361a3e5e55
Noah Houk <79422522+GunMetalBlack@users.noreply.github.com>
2022-03-12 05:37:31 +0000

    Engine Go burrrr

--- engine.py
+++ engine.py
@@ -48,36 +48,46 @@ RenderDistanceX = 3
 RenderDistanceY = 3
 
 def RenderingGameMap(stdscr):
-  for x in range(config.player_x - RenderDistanceX, config.player_x + RenderDistanceX + 1):
-    for y in range(config.player_y - RenderDistanceY, config.player_y + RenderDistanceY + 1):
-      c = config.map_01[x][y]
-      if config.player_x == x and config.player_y == y:
-        stdscr.move(y,x)
+  # Iterate Over Rendered Region
+  for i in range(-RenderDistanceX, RenderDistanceX + 1):
+    for j in range(-RenderDistanceY, RenderDistanceY + 1):
+      # Calculate Screen/World Space Coordinates
+      screen_space_x = i + RenderDistanceX
+      screen_space_y = j + RenderDistanceY
+      world_space_x = i + config.player_x
+      world_space_y = j + config.player_y
+
+      # Lookup Char from World
+      char_from_map = config.map_01[world_space_x][world_space_y]
+      # If Center of Screen, Replace World Char w/ Player Char
+      if i == 0 and j == 0:
+        stdscr.move(screen_space_y, screen_space_x)
         stdscr.addstr(imageloader.images("map_player"),curses.color_pair(3))
-      elif c == "0":
-        stdscr.move(y,x)
+      
+      # Chained 'elif' - Convert from Level Char to Render Char using 'imageloader'
+      elif char_from_map == "0":
+        stdscr.move(screen_space_y, screen_space_x)
         stdscr.addstr(imageloader.images("map_wall"))
-      elif c == "*":
-        stdscr.move(y,x)
+      elif char_from_map == "*":
+        stdscr.move(screen_space_y, screen_space_x)
         stdscr.addstr(imageloader.images("map_fill"))
-      elif c == "1":
+      elif char_from_map == "1":
         Greyrand = random.randint(236, 239)
         curses.init_pair(2,Greyrand,curses.COLOR_BLACK)
         GREY = curses.color_pair(2)
         il = random.randint(1, 3)
-        stdscr.move(y,x)
+        stdscr.move(screen_space_y, screen_space_x)
         if(il == 1):
           stdscr.addstr(imageloader.images("map_empty"),GREY)
         if(il == 2):
           stdscr.addstr(imageloader.images("map_empty01"),GREY)
         if(il == 3):
           stdscr.addstr(imageloader.images("map_empty02"),GREY)
-      #The Player is in the array
-      elif c == "3":
-        stdscr.move(y,x)
-        main.logging.error("NO run chest bad")
-        entity.Entity['chest']['Xpos'] = x
-        entity.Entity['chest']['Ypos'] = y
+      elif char_from_map == "3":
+        stdscr.move(screen_space_y, screen_space_x)
+        # This Entity Code is Hacky and Bad. I should really redo this!
+        entity.Entity['chest']['Xpos'] = world_space_x
+        entity.Entity['chest']['Ypos'] = world_space_y
         if(entity.isContainer == False):
           stdscr.addstr(entity.Entity['chest']['Graphic'],curses.color_pair(4))
         else:
